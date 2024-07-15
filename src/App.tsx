3f66d17734a8821c4eb0eeac1cf2f60d80cabd3c
Serhat Sergikaya <serhatsergikaya@gmail.com>
2024-07-15 22:59:10 +0300

    Typography

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>BUY AND SELL DIGITAL CURRENCY</h1>
+      <h1>
+        BUY AND SELL DIGITAL <span className="span-gradient">CURRENCY</span>
+      </h1>
       <h2>Trade crypto like it is meant to be</h2>
+      <h3>Lydia</h3>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure totam
         suscipit, inventore modi a soluta ratione, voluptatibus asperiores error
