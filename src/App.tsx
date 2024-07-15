38d57ba8727a28502306d80f449df6c30d1f1ba3
Serhat Sergikaya <serhatsergikaya@gmail.com>
2024-07-15 22:22:39 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>BUY AND SELL DIGITAL CURRENCY</h1>
+      <h2>Trade crypto like it is meant to be</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure totam
+        suscipit, inventore modi a soluta ratione, voluptatibus asperiores error
+        voluptatem dolores enim repellendus magnam ex aliquam nam cumque,
+        perferendis distinctio.
+      </p>
+    </>
+  );
+}
+
+export default App;
